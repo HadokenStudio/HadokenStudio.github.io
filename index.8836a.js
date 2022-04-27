69536fb5df6b30dca21ecd30051ac674df6a703f
PP_Pro <chinese1236@qq.com>
2022-04-28 02:07:33 +0800

    version 1.1.1

--- index.8836a.js
+++ index.8836a.js
@@ -1,4 +1,4 @@
-System.register(["./application.3963f.js"], function (_export, _context) {
+System.register(["./application.b0905.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.3963f.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application3963fJs) {
-      createApplication = _application3963fJs.createApplication;
+    setters: [function (_applicationB0905Js) {
+      createApplication = _applicationB0905Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
