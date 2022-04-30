bc9ab703fe2667f698c256f9d41232fa1631390f
PP_Pro <chinese1236@qq.com>
2022-04-30 09:26:23 +0800

    publish v1.1.2

--- index.dcf7f.js
+++ index.dcf7f.js
@@ -1,4 +1,4 @@
-System.register(["./application.afcf0.js"], function (_export, _context) {
+System.register(["./application.efb63.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.afcf0.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationAfcf0Js) {
-      createApplication = _applicationAfcf0Js.createApplication;
+    setters: [function (_applicationEfb63Js) {
+      createApplication = _applicationEfb63Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
